b4b174e97f5b3401adf0708b5e867b498934b7dc
Intanpur <intanps967365@gmail.com>
2025-07-15 08:34:36 +0700

    simpan

--- app/Views/adminKabupaten/templates/sidebar.php
+++ app/Views/adminKabupaten/templates/sidebar.php
@@ -62,7 +62,7 @@
   </div>
 
   <nav class="flex-grow-1 px-3 py-4">
-    <a href="<?= base_url('adminkab/dashboard') ?>" class="sidebar-link <?= ($page == 'dashboard' ? 'active' : '') ?>">
+    <a href="<?= base_url('adminkab/dashboard') ?>" class="sidebar-link <?= (isset($page) && $page == 'dashboard' ? 'active' : '') ?>">
       <i class="fas fa-tachometer-alt me-2"></i> Dashboard
     </a>
     <a href="<?= base_url('adminkab/rpjmd') ?>" class="sidebar-link <?= ($page == 'rpjmd' ? 'active' : '') ?>">
